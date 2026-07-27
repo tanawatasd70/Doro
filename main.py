da861f7bbd4c09efbe5933bf5fef4cddd6991ee3
tanawatasd70 <mpakonr@gmail.com>
2026-07-27 13:00:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1932,7 +1932,7 @@ CODE_FETCH_HEADERS = {
                   "(KHTML, like Gecko) Chrome/124.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9,th;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Referer": "https://www.google.com/",
     "Connection": "keep-alive",
     "Sec-Fetch-Dest": "document",
